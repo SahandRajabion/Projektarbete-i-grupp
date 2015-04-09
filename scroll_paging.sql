249fe376f96daf4298ba491ce85f4de59979feef
sahib <sahib@hotmail.se>
2015-04-09 13:25:13 +0200

    uppdaterat databasen

--- scroll_paging.sql
+++ scroll_paging.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Värd: 127.0.0.1
--- Tid vid skapande: 06 apr 2015 kl 21:24
+-- Tid vid skapande: 09 apr 2015 kl 13:23
 -- Serverversion: 5.6.15-log
 -- PHP-version: 5.4.24
 
@@ -27,114 +27,45 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `comments` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `CommentId` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `body` text COLLATE utf8_unicode_ci NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=136 ;
-
---
--- Dumpning av Data i tabell `comments`
---
-
-INSERT INTO `comments` (`id`, `body`, `date`) VALUES
-(130, 'nbnb', '2015-04-06 11:56:54'),
-(131, 'cvbnjkhkjhkjhkjhkhkj', '2015-04-06 12:46:00'),
-(132, 'sdsd', '2015-04-06 13:46:50'),
-(133, 'sdfdsfsldkfÃ¶lsd', '2015-04-06 13:47:08'),
-(134, 'hejhejhej', '2015-04-06 13:51:47'),
-(135, 'hejhejhej', '2015-04-06 13:53:16');
+  `id` int(10) NOT NULL,
+  PRIMARY KEY (`CommentId`),
+  KEY `PostId` (`id`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=151 ;
 
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `items`
+-- Tabellstruktur `feed`
 --
 
-CREATE TABLE IF NOT EXISTS `items` (
+CREATE TABLE IF NOT EXISTS `feed` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `description` text COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=11 ;
-
---
--- Dumpning av Data i tabell `items`
---
-
-INSERT INTO `items` (`id`, `title`, `description`) VALUES
-(2, 'Lamborghini', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'),
-(3, 'Ferrari', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'),
-(4, 'BMW', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'),
-(5, 'Mercedes benz', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'),
-(6, 'Bugatti', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'),
-(7, 'Porsche', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'),
-(8, 'Toyota', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'),
-(9, 'Audi', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'),
-(10, 'Honda', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.');
-
--- --------------------------------------------------------
-
---
--- Tabellstruktur `pictures`
---
-
-CREATE TABLE IF NOT EXISTS `pictures` (
-  `imgId` int(11) NOT NULL AUTO_INCREMENT,
-  `imgName` varchar(255) NOT NULL,
-  `Title` varchar(500) NOT NULL,
-  PRIMARY KEY (`imgId`),
-  UNIQUE KEY `imgName` (`imgName`),
-  KEY `imgName_2` (`imgName`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=185 ;
-
---
--- Dumpning av Data i tabell `pictures`
---
-
-INSERT INTO `pictures` (`imgId`, `imgName`, `Title`) VALUES
-(183, 'Chrysanthemum.jpg', 'hbhb'),
-(184, 'Desert.jpg', 'nbnmbnmb');
-
--- --------------------------------------------------------
-
---
--- Tabellstruktur `posts`
---
-
-CREATE TABLE IF NOT EXISTS `posts` (
-  `PostId` int(10) NOT NULL AUTO_INCREMENT,
-  `Post` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `imgName` varchar(255) DEFAULT NULL,
+  `Title` varchar(255) DEFAULT NULL,
+  `Post` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `code` varchar(255) DEFAULT NULL,
   `Date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`PostId`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `imgName` (`imgName`,`code`),
+  KEY `id_2` (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=34 ;
 
 --
--- Dumpning av Data i tabell `posts`
+-- Restriktioner för dumpade tabeller
 --
 
-INSERT INTO `posts` (`PostId`, `Post`, `Date`) VALUES
-(1, 'Test 1Test 1Test 1Test 1Test 1Test 1Test 1Test 1Test 1Test 1Test 1Test 1Test 1Test 1Test 1Test 1Test 1Test 1Test 1Test 1Test 1', '2015-04-06 13:42:30'),
-(2, 'Testar igenTestar igenTestar igenTestar igenTestar igenTestar igenTestar igenTestar igenTestar igenTestar igenTestar igenTestar igenTestar igenTestar igenTestar igenTestar igen', '2015-04-06 13:42:30');
+--
+-- Restriktioner för tabell `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `dsdsaads` FOREIGN KEY (`id`) REFERENCES `feed` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-
--- --------------------------------------------------------
-
---
--- Tabellstruktur `videos`
---
-
-CREATE TABLE IF NOT EXISTS `videos` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `code` varchar(20) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
-
---
--- Dumpning av Data i tabell `videos`
---
