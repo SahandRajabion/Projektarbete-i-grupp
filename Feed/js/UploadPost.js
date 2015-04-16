7fe97d17d73c5819a227ea4b081835077c357eb4
tn222eb <tn222eb@student.lnu.se>
2015-04-16 17:56:30 +0200

    Fixat liten bug som har med uppladning +1:
    Jojo

--- Feed/js/UploadPost.js
+++ Feed/js/UploadPost.js
@@ -13,15 +13,8 @@ $(document).ready(function() {
 	 { 		
 		e.preventDefault(); 
 
-		if(working) 
-		{
-		 	return false;
-		}
-
-		else 
-		{
+		if (working == false) {
 			$(this).ajaxSubmit(options);
-			working = true;	
 		}
 	}); 
 		
@@ -29,54 +22,54 @@ $(document).ready(function() {
 //function after succesful file upload (when server response)
 function afterSuccess(responseText, statusText, xhr, $form)
 {
+	$('#progressbox').fadeOut(); //hide progress bar
+
 	working = false;
-
-	$('#submit-btn').show(); //hide submit button
-	$('#progressbox').delay( 3000 ).fadeOut(); //hide progress bar
-
 }
 
 //function to check file size before uploading.
-function beforeSubmit(){
-
-		    //check whether browser fully supports all File API
-
-		if( !$('#FileInput').val() && !$('#Message').val() ) //check empty input filed
+function beforeSubmit()
+{
+		// check whether browser fully supports all File API
+		if (working == false) 
 		{
-			$("#output").html("<br>Dela något!");
-			return false
-		}
+			if( !$('#FileInput').val() && !$('#Message').val() ) //check empty input filed
+			{
+				$("#output").html("<br><p>Dela något!</p>");
+				return false;
+			}
 
-		if (!$('#FileInput').val().length == 0)
-		{
+			if (!$('#FileInput').val().length == 0)
+			{
 
-		var fsize = $('#FileInput')[0].files[0].size; //get file size
-		var ftype = $('#FileInput')[0].files[0].type; // get file type
-		
+			var fsize = $('#FileInput')[0].files[0].size; //get file size
+			var ftype = $('#FileInput')[0].files[0].type; // get file type
+			
 
-		//allow file types 
-		switch(ftype)
-        {
-            case 'image/png': 
-			case 'image/gif': 
-			case 'image/jpeg': 
-                break;
-            default:
-                $("#output").html("<b>"+ftype+"</b> Filen är av en fel typ!");
+			//allow file types 
+			switch(ftype)
+	        {
+	            case 'image/png': 
+				case 'image/gif': 
+				case 'image/jpeg': 
+	                break;
+	            default:
+	                $("#output").html("<b>"+ftype+"</b> Filen är av en fel typ!");
+					return false
+	        }
+			
+			
+			if(fsize>5242880) 
+			{
+				$("#output").html("<b>"+bytesToSize(fsize) +"</b>Filen är för stor <br />Filen kan vara max 5 MB.");
 				return false
-        }
-		
-		
-		if(fsize>5242880) 
-		{
-			$("#output").html("<b>"+bytesToSize(fsize) +"</b>Filen är för stor <br />Filen kan vara max 5 MB.");
-			return false
+			}
 		}
-				
+		
+		working = true;
 		$('#progressbox').delay( 1000 ).fadeOut();
-		$("#output").html("");  
-		} 
-
+		$("#output").html("");  	
+	}
 }
 
 //progress bar function
