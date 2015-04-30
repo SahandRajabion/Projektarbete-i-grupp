0e97b26d3d045aa2cca9340f2ffc75984358df42
tn222eb <tn222eb@student.lnu.se>
2015-04-30 21:09:52 +0200

    copy paste roten till allt ont

--- Feed/CheckForRemovedPosts.php
+++ Feed/CheckForRemovedPosts.php
@@ -2,8 +2,8 @@
 
 require_once("Model/Dao/PostRepository.php");
 
-if (isset($_POST['arrayOfPostIds']) && empty($_POST['arrayOfPostIds']) == false && count($_POST['arrayOfPostIds']) > 1
-	&& isset($_POST['course_id']) && empty($_POST['course_id']) == false && count($_POST['course_id']) > 1) {
+if (isset($_POST['arrayOfPostIds']) && empty($_POST['arrayOfPostIds']) == false
+	&& isset($_POST['course_id']) && empty($_POST['course_id']) == false) {
 
 	$sitePostIds = $_POST['arrayOfPostIds']; 
 	$course_id = $_POST['course_id'];
